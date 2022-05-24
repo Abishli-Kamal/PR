b0d26785ad74337c935cf2a67f1100e3f60ca52d
Abishli-Kamal <tu7ldxfzy@code.edu.az>
2022-05-24 17:54:40 +0400

    first commit

---
+++ PraktikaImport/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PraktikaImport.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
